3447f937208398a1ef8fbf4e81f8425bab6f954f
JosephNguyenNazareth <iohkgn@outlook.com>
2017-09-01 07:34:28 +0700

    test file

--- README.md
+++ README.md
@@ -9,6 +9,10 @@ Naive algorithm costs O($$n^3$$). Thanks to Strassen, we can reach to O($$n^(2.8
 Strassen algorithm has a special feature: Instead of doing multiplication by nxnxn as common is mathematics, this turn to a whole new method.
 The base case is 2x2 matrix, which is usually be multiplied in style of 2x2x2 = 8 loops; on the other hand, Strassen use his greedy to cut off one loop, which mean, in his algorithm, there are only 7 loops!
 The most interesting matter is how he can find and research in that way; however, we can save time for this innovation.
+#### c) Implementation
+Strassen algorithm is implemented on C++, using package "chrono" to couting time with milisecond accuracy.
+At first, user enters the size of the matrix. <b>Note</b>: size must be 2 exponent.
+After that, select the algorithm user wants to test, and now, the algorithm runs. For a while (seconds to minutes, depend on size), users will know the time elapsed of each algorithm.
 
 
 ### 2. Coppersmith - Winograd algorithm
--- SubCubic.cpp
+++ SubCubic.cpp
@@ -62,7 +62,7 @@ public:
   void Print() {
     for (int index = 0; index < row; index++) {
       for (int ikimashou = 0; ikimashou < col; ikimashou++) {
-        std::cout << std::setw(10) << data[index][ikimashou];
+        std::cout << std::setw(7) << data[index][ikimashou];
       }
       std::cout << std::endl;
     }
@@ -113,98 +113,97 @@ public:
     }
   }
   void StrassenMultiplyCore(const matrix &MatA, const matrix &MatB) {
-    matrix MatC(MatB.row, MatB.row);
     if (MatB.row == 1) { // base case
       this->data[0][0] = MatA.data[0][0] * MatB.data[0][0];
     } else {
       unsigned int newSize = MatB.row / 2;
+
       matrix MatB11(newSize, newSize), MatB12(newSize, newSize),
           MatB21(newSize, newSize), MatB22(newSize, newSize),
+
           //***********************************************//
+
           MatA11(newSize, newSize), MatA12(newSize, newSize),
           MatA21(newSize, newSize), MatA22(newSize, newSize),
+
           //***********************************************//
+
           MatC11(newSize, newSize), MatC12(newSize, newSize),
           MatC21(newSize, newSize), MatC22(newSize, newSize);
-      //***********************************************//
-      for (int index = 0; index < MatB.row; index++) {
-        for (int ikimashou = 0; ikimashou < MatB.row; ikimashou++) {
-          if (index < newSize && ikimashou < newSize) { // top left submatrix
-            MatB11.data[index][ikimashou] = MatB.data[index][ikimashou];
-            MatA11.data[index][ikimashou] = MatA.data[index][ikimashou];
-          } else if (index < newSize &&
-                     ikimashou > newSize - 1) { // top right submatrix
-            MatB12.data[index][ikimashou - newSize] =
-                MatB.data[index][ikimashou];
-            MatA12.data[index][ikimashou - newSize] =
-                MatA.data[index][ikimashou];
-          } else if (index > newSize - 1 &&
-                     ikimashou < newSize) { // bottom left submatrix
-            MatB21.data[index - newSize][ikimashou] =
-                MatB.data[index][ikimashou];
-            MatA21.data[index - newSize][ikimashou] =
-                MatA.data[index][ikimashou];
-          } else { // bottom right matrix
-            MatB22.data[index - newSize][ikimashou - newSize] =
-                MatB.data[index][ikimashou];
-            MatA22.data[index - newSize][ikimashou - newSize] =
-                MatA.data[index][ikimashou];
-          }
+
+      for (int index = 0; index < newSize; index++) {
+        for (int ikimashou = 0; ikimashou < newSize; ikimashou++) {
+
+          // top left
+          MatB11.data[index][ikimashou] = MatB.data[index][ikimashou];
+          MatA11.data[index][ikimashou] = MatA.data[index][ikimashou];
+
+          // top right
+          MatB12.data[index][ikimashou] = MatB.data[index][ikimashou + newSize];
+          MatA12.data[index][ikimashou] = MatA.data[index][ikimashou + newSize];
+
+          // bottom left
+          MatB21.data[index][ikimashou] = MatB.data[index + newSize][ikimashou];
+          MatA21.data[index][ikimashou] = MatA.data[index + newSize][ikimashou];
+
+          // bottom right
+          MatB22.data[index][ikimashou] =
+              MatB.data[index + newSize][ikimashou + newSize];
+          MatA22.data[index][ikimashou] =
+              MatA.data[index + newSize][ikimashou + newSize];
         }
       }
 
       // according to Strassen work, he found there are 7 equations to calculate
       // that is able to multiply matrix
-      matrix *P = new matrix[7];
-      for (unsigned int kitta = 0; kitta < 7; kitta++)
-        P[kitta].SetSize(newSize, newSize);
+      matrix P0(newSize, newSize), P1(newSize, newSize), P2(newSize, newSize), P3(newSize, newSize), P4(newSize, newSize), P5(newSize, newSize), P6(newSize, newSize);
 
       // P0 = A11*(B12-B22) //
-      P[0].StrassenMultiplyCore(MatA11, MatB12.Subtract(MatB22));
+      P0.StrassenMultiplyCore(MatA11, MatB12.Subtract(MatB22));
+
       // P1 = A11*(A11+B22) //
-      P[1].StrassenMultiplyCore(MatA11.Add(MatA12), MatB22);
+      P1.StrassenMultiplyCore(MatA11.Add(MatA12), MatB22);
+
       // P2 = A21*(A22+B11) //
-      P[2].StrassenMultiplyCore(MatA21.Add(MatA22), MatB11);
+      P2.StrassenMultiplyCore(MatA21.Add(MatA22), MatB11);
+
       // P3 = A22*(B21-B11) //
-      P[3].StrassenMultiplyCore(MatA22, MatB21.Subtract(MatB11));
+      P3.StrassenMultiplyCore(MatA22, MatB21.Subtract(MatB11));
+
       // P4 = (A11+A22)*(B11+B22) //
-      P[4].StrassenMultiplyCore(MatA11.Add(MatA22), MatB11.Add(MatB22));
+      P4.StrassenMultiplyCore(MatA11.Add(MatA22), MatB11.Add(MatB22));
+
       // P5 = (A12-A22)*(B21-B22) //
-      P[5].StrassenMultiplyCore(MatA12.Subtract(MatA22), MatB21.Add(MatB22));
+      P5.StrassenMultiplyCore(MatA12.Subtract(MatA22), MatB21.Add(MatB22));
+
       // P6 = (A11-A21)*(B11+B12) //
-      P[6].StrassenMultiplyCore(MatA11.Subtract(MatA21), MatB11.Add(MatB12));
+      P6.StrassenMultiplyCore(MatA11.Subtract(MatA21), MatB11.Add(MatB12));
+
 
       // 4 submatrices of result matrix are calculated as below
       // how did Strassen think?
-      MatC11 = P[4].Add(P[3]);
-      MatC11 = MatC11.Subtract(P[1]);
-      MatC11 = MatC11.Add(P[5]);
+      MatC11 = ((P4.Add(P3)).Subtract(P1)).Add(P5);
 
-      MatC12 = P[0].Add(P[1]);
+      MatC12 = P0.Add(P1);
 
-      MatC21 = P[2].Add(P[3]);
+      MatC21 = P2.Add(P3);
 
-      MatC22 = P[4].Add(P[0]);
-      MatC22 = MatC22.Subtract(P[2]);
-      MatC22 = MatC22.Subtract(P[6]);
+      MatC22 = ((P4.Add(P0)).Subtract(P2)).Subtract(P6);
 
       // assign 4 submatrices to the result matrix
-      for (int index = 0; index < MatB.row; index++) {
-        for (int ikimashou = 0; ikimashou < MatB.row; ikimashou++) {
-          if (index < newSize && ikimashou < newSize) { // top left submatrix
-            this->data[index][ikimashou] = MatC11.data[index][ikimashou];
-          } else if (index < newSize &&
-                     ikimashou > newSize - 1) { // top right matrix
-            this->data[index][ikimashou] =
-                MatC12.data[index][ikimashou - newSize];
-          } else if (index > newSize - 1 &&
-                     ikimashou < newSize) { // bottom left matrix
-            this->data[index][ikimashou] =
-                MatC21.data[index - newSize][ikimashou];
-          } else { // bottom right matrix
-            this->data[index][ikimashou] =
-                MatC22.data[index - newSize][ikimashou - newSize];
-          }
+      for (int index = 0; index < newSize; index++) {
+        for (int ikimashou = 0; ikimashou < newSize; ikimashou++) {
+          // top left submatrix
+          this->data[index][ikimashou] = MatC11.data[index][ikimashou];
+          // top right matrix
+          this->data[index][ikimashou + newSize] =
+              MatC12.data[index][ikimashou];
+          // bottom left matrix
+          this->data[index + newSize][ikimashou] =
+              MatC21.data[index][ikimashou];
+          // bottom right matrix
+          this->data[index + newSize][ikimashou + newSize] =
+              MatC22.data[index][ikimashou];
         }
       }
     }
@@ -252,29 +251,33 @@ int main(int argc, char **argv) {
   chronoTime startTime, endTime;
 
   srand(time(NULL));
-  unsigned int userSize = 0;
+  unsigned int userSize = 2048;
+  /*/====================------USER_SELECTION PART------====================//
   std::cout << "Size of square matrix: ";
   std::cin >> userSize;
 
   unsigned int selection;
-  std::cout << "Select algorithm: 1 for Naive, 2 for Strassen";
+  std::cout << "Select algorithm: 1 for Naive, 2 for Strassen: ";
   std::cin >> selection;
+  //==================------END USER_SELECTION PART------==================/*/
 
-  matrix<int> MatA(userSize, userSize);
+  matrix<float> MatA(userSize, userSize);
   MatA.SetRandomValue();
+  MatA.Print();
 
-  matrix<int> MatB(userSize, userSize);
+  matrix<float> MatB(userSize, userSize);
   MatB.SetRandomValue();
+  MatB.Print();
 
-  startTime = std::chrono::system_clock::now();
+  /*startTime = std::chrono::system_clock::now();
 
   switch (selection) {
   case 1: {
-    matrix<int> MatD(userSize,userSize);
+    matrix<int> MatD(userSize, userSize);
     MatD.BasicMultiply(MatA, MatB);
     break;
   }
-  case 2:{
+  case 2: {
     matrix<int> MatC(userSize, userSize);
     MatC.StrassenMultiply(MatA, MatB);
     break;
@@ -283,6 +286,6 @@ int main(int argc, char **argv) {
   endTime = std::chrono::system_clock::now();
 
   std::chrono::duration<double> elapsed_seconds = endTime - startTime;
-  std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";
+  std::cout << "Elapsed time: " << elapsed_seconds.count() << "s\n";*/
   return 0;
 }
